e55c18ff9cc09f637ef7d3318ad0aca6fe4efd5e
miriamjaser <miriam.jaser@tngtech.com>
2024-11-15 14:18:51 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,23 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Miriam Jaser',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'automatic deployment of software'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'not sure',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'let's see'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'long distance hiking'
 		]
 	}
 });
